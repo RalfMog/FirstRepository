de678c6df12801bca5d31dd3987633c4570cbac0
Yuriy <yurispb.mogilevich@gmail.com>
2022-09-25 01:05:46 +0500

    user2 changed Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello, World!12345");
         System.out.println("Hello, World!12345");
         System.out.println("Hello, World!12345");
+        System.out.println("Hello, World!12345");
     }
 }
