681b5b5d16c1ee36f5e06480ec39d717a610f14f
Yuriy <yurispb.mogilevich@gmail.com>
2022-09-25 11:37:25 +0500

    change Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!12345");
-        System.out.println("Hello, World!12345");
-        System.out.println("Hello, World!12345");
-        System.out.println("Hello, World!12345");
-        System.out.println("user_2");
+        
+        String user1 =
+                "Good morning!" +
+                "From branch add-feature1;
     }
 }
